0576a7af687a8fad556f876c3bbbb833d8a44b1a
victorbaldin56 <victor.baldin.2005@gmail.com>
2025-04-24 13:11:19 +0000

    Revert "add openmp and reduce block size to 2 for more effective parallelism"
    This reverts commit 606026ad251789df8d6ec64e3f148c41dddeaa1c.

--- include/sgemm/matmul.h
+++ include/sgemm/matmul.h
@@ -10,14 +10,8 @@ void matmulNaive(size_t m, size_t n, size_t k, const float* __restrict__ a,
                  const float* __restrict__ b, float* __restrict__ c);
 void matmulAvx2(size_t m, size_t n, size_t k, const float* __restrict__ a,
                 const float* __restrict__ b, float* __restrict__ c);
-void matmulAvx2Parallel(size_t m, size_t n, size_t k,
-                        const float* __restrict__ a,
-                        const float* __restrict__ b, float* __restrict__ c);
 void matmulAvx512(size_t m, size_t n, size_t k, const float* __restrict__ a,
                   const float* __restrict__ b, float* __restrict__ c);
-void matmulAvx512Parallel(size_t m, size_t n, size_t k,
-                          const float* __restrict__ a,
-                          const float* __restrict__ b, float* __restrict__ c);
 
 #ifdef __cplusplus
 }
--- tests/test.cc
+++ tests/test.cc
@@ -59,18 +59,8 @@ TEST_F(MatmulTest, matmulAvx2) {
   ASSERT_TRUE(isResultsEqual());
 }
 
-TEST_F(MatmulTest, matmulAvx2Parallel) {
-  matmulAvx2Parallel(kM, kN, kK, a_, b_, c_);
-  ASSERT_TRUE(isResultsEqual());
-}
-
 TEST_F(MatmulTest, matmulAvx512) {
   matmulAvx512(kM, kN, kK, a_, b_, c_);
-  ASSERT_TRUE(isResultsEqual());
-}
-
-TEST_F(MatmulTest, matmulAvx512Parallel) {
-  matmulAvx512Parallel(kM, kN, kK, a_, b_, c_);
   ASSERT_TRUE(isResultsEqual());
 }
 
