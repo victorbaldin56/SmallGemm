72df1a7e92e6cb96e891cd53286ae6d2c5ce1deb
victorbaldin56 <victor.baldin.2005@gmail.com>
2025-04-24 18:52:01 +0000

    bench different sizes

--- bench/bench.cc
+++ bench/bench.cc
@@ -1,6 +1,8 @@
 #include <immintrin.h>
 
+#include <cstdlib>
 #include <random>
+#include <vector>
 
 #include "benchmark/benchmark.h"
 #include "openblas/cblas.h"
@@ -8,15 +10,56 @@
 
 namespace {
 
-constexpr std::size_t kM = 0x20;
-constexpr std::size_t kN = 0x20;
-constexpr std::size_t kK = 0x10;
+template <typename T, std::size_t Alignment>
+struct AlignedAllocator {
+  using value_type = T;
 
-constexpr unsigned kSeed = 0xdeadbeef;
+  AlignedAllocator() noexcept = default;
+
+  template <typename U>
+  AlignedAllocator(const AlignedAllocator<U, Alignment>&) noexcept {}
+
+  T* allocate(std::size_t n) {
+    auto ptr = std::aligned_alloc(Alignment, n * sizeof(T));
+    if (ptr == nullptr) {
+      throw std::bad_alloc();
+    }
+    return static_cast<T*>(ptr);
+  }
+
+  void deallocate(T* p, std::size_t) noexcept { std::free(p); }
+
+  template <typename U>
+  struct rebind {
+    using other = AlignedAllocator<U, Alignment>;
+  };
+
+  using is_always_equal = std::true_type;
+};
+
+template <typename T1, std::size_t A1, typename T2, std::size_t A2>
+bool operator==(const AlignedAllocator<T1, A1>&,
+                const AlignedAllocator<T2, A2>&) noexcept {
+  return A1 == A2;
+}
+
+template <typename T1, std::size_t A1, typename T2, std::size_t A2>
+bool operator!=(const AlignedAllocator<T1, A1>& a1,
+                const AlignedAllocator<T2, A2>& a2) noexcept {
+  return !(a1 == a2);
+}
 
 class MatmulBenchmark : public benchmark::Fixture {
  public:
   void SetUp(const benchmark::State& state) noexcept override {
+    m_ = state.range(0);
+    n_ = state.range(1);
+    k_ = state.range(2);
+
+    a_.resize(k_ * m_);
+    b_.resize(n_ * k_);
+    c_.resize(m_ * n_);
+
     std::mt19937_64 rng(kSeed);
     std::uniform_real_distribution<float> dist(0, 1);
     std::generate(std::begin(a_), std::end(a_), [&]() { return dist(rng); });
@@ -29,7 +72,7 @@ class MatmulBenchmark : public benchmark::Fixture {
     state.PauseTiming();
     auto start = __rdtsc();
     for (auto i = 0; i < 100000; ++i) {
-      matmul_func(kM, kN, kK, a_, b_, c_);
+      matmul_func(m_, n_, k_, a_.data(), b_.data(), c_.data());
     }
     state.counters["cpu cycles"] = __rdtsc() - start;
   }
@@ -41,27 +84,33 @@ class MatmulBenchmark : public benchmark::Fixture {
                 b, n, 0.0f, c, n);
   }
 
+ private:
+  static constexpr unsigned kSeed = 0xdeadbeef;
+  using AlignedMatrix = std::vector<float, AlignedAllocator<float, 64>>;
+
  protected:
-  alignas(64) float a_[kK * kM];
-  alignas(64) float b_[kN * kK];
-  alignas(64) float c_[kM * kN];
+  std::size_t m_;
+  std::size_t n_;
+  std::size_t k_;
+  AlignedMatrix a_;
+  AlignedMatrix b_;
+  AlignedMatrix c_;
 };
 }  // namespace
 
-BENCHMARK_F(MatmulBenchmark, Naive)(benchmark::State& state) {
-  measureMatmul<matmulNaive>(state);
-}
+#define REGISTER_MATMUL_BENCHMARK(name, func)                           \
+  BENCHMARK_DEFINE_F(MatmulBenchmark, name)(benchmark::State & state) { \
+    measureMatmul<func>(state);                                         \
+  }                                                                     \
+  BENCHMARK_REGISTER_F(MatmulBenchmark, name)                           \
+      ->Args({0x10, 0x10, 0x10})                                        \
+      ->Args({0x20, 0x20, 0x10})                                        \
+      ->Args({0x40, 0x40, 0x20})                                        \
+      ->Args({0x40, 0x40, 0x40});
 
-BENCHMARK_F(MatmulBenchmark, AVX2)(benchmark::State& state) {
-  measureMatmul<matmulAvx2>(state);
-}
-
-BENCHMARK_F(MatmulBenchmark, AVX512)(benchmark::State& state) {
-  measureMatmul<matmulAvx512>(state);
-}
-
-BENCHMARK_F(MatmulBenchmark, OpenBLAS)(benchmark::State& state) {
-  measureMatmul<matmulOpenblas>(state);
-}
+REGISTER_MATMUL_BENCHMARK(Naive, matmulNaive);
+REGISTER_MATMUL_BENCHMARK(AVX2, matmulAvx2);
+REGISTER_MATMUL_BENCHMARK(AVX512, matmulAvx512);
+REGISTER_MATMUL_BENCHMARK(OpenBLAS, matmulOpenblas);
 
 BENCHMARK_MAIN();
